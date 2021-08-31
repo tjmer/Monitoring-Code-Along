fdee6523a9698e058bd037a2014c02c964341ab3
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 12:26:46 -0600

    made changes

--- server.js
+++ server.js
@@ -3,15 +3,32 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: ''
+    accessToken: 'bf1d4ede430d4e13b8b742c64b9f0dcd',
+    captureUncaught: true,
+    captureUnhandledRejections: true
 })
 
+const students =[]
 const app = express()
+const port = process.env.PORT || 4545
+
 
 app.get('/', (req, res)=> {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully.')
 })
 
-const port = process.env.PORT || 4545
+app.post('/api/student', (req,res)=> {
+    const{name}=req.body
+    name = name.trim()
+    
+    students.push(name)
+    
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+    
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, ()=> console.log(`up on ${port}`))
