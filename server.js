933fd1ff88253ffec9684367c9766ffaa9e45bda
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 12:30:28 -0600

    added stuff

--- server.js
+++ server.js
@@ -3,32 +3,46 @@ const path = require('path')
 const Rollbar = require('rollbar')
 
 let rollbar = new Rollbar({
-    accessToken: 'bf1d4ede430d4e13b8b742c64b9f0dcd',
+    accessToken: '9f8c850a503b4b3993660562d62e5f3f',
     captureUncaught: true,
     captureUnhandledRejections: true
 })
 
-const students =[]
 const app = express()
-const port = process.env.PORT || 4545
 
+app.use(express.json())
+app.use('/style', express.static('./public/styles.css'))
 
-app.get('/', (req, res)=> {
+let students = []
+
+app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('html file served successfully.')
 })
 
-app.post('/api/student', (req,res)=> {
-    const{name}=req.body
+app.post('/api/student', (req, res)=>{
+    let {name} = req.body
     name = name.trim()
-    
-    students.push(name)
-    
-    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
-    
-    res.status(200).send(students)
+
+    const index = students.findIndex(studentName=> studentName === name)
+
+    if(index === -1 && name !== ''){
+        students.push(name)
+        rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+        res.status(200).send(students)
+    } else if (name === ''){
+        rollbar.error('No name given')
+        res.status(400).send('must provide a name.')
+    } else {
+        rollbar.error('student already exists')
+        res.status(400).send('that student already exists')
+    }
+
 })
 
+const port = process.env.PORT || 4545
+
+// rolly
 app.use(rollbar.errorHandler())
 
-app.listen(port, ()=> console.log(`up on ${port}`))
+app.listen(port, () => console.log(`Take us to warp ${port}!`))
