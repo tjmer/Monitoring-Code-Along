dbad214ef3e0f9a27660ca30a265be7e0a2a3483
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 11:37:09 -0600

    Added changes

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: ''
+})
+
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
+const port = process.env.PORT || 4545
+
+app.listen(port, ()=> console.log(`up on ${port}`))
